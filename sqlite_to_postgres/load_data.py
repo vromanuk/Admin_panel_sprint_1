bb4097d209d2ba498ca1c5f6d729b93e3d98ad84
Vlad Romaniuk <v.romaniuk@orderry.com>
2021-05-31 22:47:28 +0300

    Create initial project setup (git-hooks, venv, utils files)

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -14,7 +14,13 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     # postgres_saver.save_all_data(data)
 
 
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'postgres', 'password': 1234, 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+if __name__ == "__main__":
+    dsl = {
+        "dbname": "movies_database",
+        "user": "postgres",
+        "password": 1234,
+        "host": "127.0.0.1",
+        "port": 5432,
+    }
+    with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
