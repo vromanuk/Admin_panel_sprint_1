5017388def6c8b60b5bfe2fc99b9e092ff9eb781
Vlad Romaniuk <v.romaniuk@orderry.com>
2021-06-07 21:33:46 +0300

    Fix schema issues

--- schema_design/schema.py
+++ schema_design/schema.py
@@ -12,13 +12,18 @@ conn = psycopg2.connect(
 with conn.cursor() as cur:
     cur.execute(
         """
-    -- Фильм – заголовок, содержание, дата создания, возрастной ценз, режиссеры, актеры, сценаристы, жанры, ссылка.
+            CREATE SCHEMA IF NOT EXISTS content;
+        """
+    )
+
+    cur.execute(
+        """
         CREATE TABLE IF NOT EXISTS content.movies (
             id uuid PRIMARY KEY,
-            title VARCHAR(255) UNIQUE NOT NULL,
+            title VARCHAR(255) NOT NULL,
             plot TEXT,
             creation_date DATE NOT NULL,
-            rating VARCHAR (10)
+            mpa_film_rating VARCHAR (10)
             imdb_rating FLOAT,
             link VARCHAR(255) NOT NULL,
         );
@@ -27,31 +32,37 @@ with conn.cursor() as cur:
 
     cur.execute(
         """
-    -- Актер, Режиссер, Сценарист — Имя, фамилия, его фильмы
         CREATE TABLE IF NOT EXISTS content.people (
             id uuid PRIMARY KEY,
             first_name VARCHAR(45) NOT NULL,
             last_name VARCHAR(45) NOT NULL,
-            role VARCHAR(45) NOT NULL,
         );
     """
     )
 
     cur.execute(
         """
-    -- Промежуточная таблица для Movie/People
+        CREATE TABLE IF NOT EXISTS content.roles (
+            id serial PRIMARY KEY,
+            role VARCHAR(45) UNIQUE NOT NULL,
+        );
+    """
+    )
+
+    cur.execute(
+        """
         CREATE TABLE IF NOT EXISTS content.movie_people (
             person_id uuid NOT NULL,
             movie_id uuid NOT NULL,
             FOREIGN KEY (person_id) REFERENCES content.people (id) ON UPDATE CASCADE,
             FOREIGN KEY (movie_id) REFERENCES content.movies (id) ON UPDATE CASCADE,
+            FOREIGN KEY (role_id) REFERENCES content.roles (id)
         );
     """
     )
 
     cur.execute(
         """
-    -- Жанр — Описание
         CREATE TABLE IF NOT EXISTS content.genres (
             id serial PRIMARY KEY,
             genre VARCHAR(45) UNIQUE NOT NULL,
@@ -61,7 +72,6 @@ with conn.cursor() as cur:
 
     cur.execute(
         """
-    -- Промежуточная таблица для Movie/Genre
         CREATE TABLE IF NOT EXISTS content.movie_genres (
             movie_id uuid NOT NULL,
             genre_id INT NOT NULL,
