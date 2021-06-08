24301234821bb8066bb8f241fb1836a4c2f2edce
Vlad Romaniuk <v.romaniuk@orderry.com>
2021-06-08 20:25:26 +0300

    Fix schema issues

--- schema_design/schema.py
+++ schema_design/schema.py
@@ -42,9 +42,15 @@ with conn.cursor() as cur:
 
     cur.execute(
         """
+        CREATE TYPE person_roles AS ENUM ('Actor','Director', 'Writer');
+        """
+    )
+
+    cur.execute(
+        """
         CREATE TABLE IF NOT EXISTS content.roles (
             id serial PRIMARY KEY,
-            role VARCHAR(45) UNIQUE NOT NULL,
+            role person_roles DEFAULT 'Actor' NOT NULL,
         );
     """
     )
@@ -54,6 +60,7 @@ with conn.cursor() as cur:
         CREATE TABLE IF NOT EXISTS content.movie_people (
             person_id uuid NOT NULL,
             movie_id uuid NOT NULL,
+            UNIQUE (movie_id, person_id)
             FOREIGN KEY (person_id) REFERENCES content.people (id) ON UPDATE CASCADE,
             FOREIGN KEY (movie_id) REFERENCES content.movies (id) ON UPDATE CASCADE,
             FOREIGN KEY (role_id) REFERENCES content.roles (id)
@@ -75,6 +82,7 @@ with conn.cursor() as cur:
         CREATE TABLE IF NOT EXISTS content.movie_genres (
             movie_id uuid NOT NULL,
             genre_id INT NOT NULL,
+            UNIQUE (movie_id, genre_id)
             FOREIGN KEY (movie_id) REFERENCES content.movies (id) ON UPDATE CASCADE,
             FOREIGN KEY (genre_id) REFERENCES content.genres (id) ON UPDATE CASCADE,
         );
